de5b0ffa54ef81c1b34fa497c72fb01da255ee01
Jamie Robert Dawson <jamierobertdawson@gmail.com>
2016-08-03 10:59:59 -0400

    Encrypt Example

---
+++ C_TripleDesEncryption/Form1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Security.Cryptography;
+
+namespace C_TripleDesEncryption
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        //Without the key, you can't encrypt.
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MD5CryptoServiceProvider md5 = new MD5CryptoServiceProvider();
+            UTF8Encoding utf8 = new UTF8Encoding();
+            TripleDESCryptoServiceProvider tDES = new TripleDESCryptoServiceProvider();
+            tDES.Key = md5.ComputeHash(utf8.GetBytes(textBox1.Text));
+            tDES.Mode = CipherMode.ECB;
+            tDES.Padding = PaddingMode.PKCS7;
+            ICryptoTransform trans = tDES.CreateEncryptor();
+            textBox3.Text = BitConverter.ToString(trans.TransformFinalBlock(utf8.GetBytes(textBox2.Text), 0, utf8.GetBytes(textBox2.Text).Length));
+
+        }
+    }
+}
